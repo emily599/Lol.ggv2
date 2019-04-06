f528595c598fb9d0b549f75997efec08b59305f3
Emily Doan <emilydoan52@gmail.com>
2019-04-06 10:53:00 -0700

    change in server

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 app.use(morgan("common"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lol");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lol", {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
 
 app.get("*", (req, res) => {
   rres.sendFile(path.join(__dirname, "client", "build", "index.html"));
