e8b8c37344f471ba42d00d4d902159c27fc4f276
Emily Doan <emilydoan52@gmail.com>
2019-05-10 23:30:32 -0700

    carousel issue

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lol", {
 });
 
 app.get("*", (req, res) => {
-  rres.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
